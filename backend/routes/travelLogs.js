d74d96f593f922906da348fc55853d7812de1c28
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-01-14 17:32:12 +0100

    Backend: validate incoming travel log and create travel log

--- backend/routes/travelLogs.js
+++ backend/routes/travelLogs.js
@@ -12,8 +12,22 @@ router.get('/', jwtCheck, checkUser, async (req, res) => {
   res.json({ msg: 'Get all travel logs', data: logs });
 });
 
-router.post('/', jwtCheck, (req, res) => {
-  res.json({ msg: 'Endpoint to create travel log' });
+router.post('/', jwtCheck, checkUser, async (req, res) => {
+  console.log(req.body);
+  // Validate if all fields are filled in
+  for (let prop in req.body) {
+    if (!req.body[prop]) {
+      console.log('Not filled in');
+      return res.status(400).json({ msg: 'All fields are required.' });
+    }
+  }
+  const { _id } = req.loggedInUser;
+  const { title, place, description } = req.body;
+
+  // Create travel log if all fields are filled in
+  const log = new TravelLog({ title, place, description, owner: _id });
+  await log.save();
+  res.status(201).json({ msg: 'Endpoint to create travel log' });
 });
 
 module.exports = router;
