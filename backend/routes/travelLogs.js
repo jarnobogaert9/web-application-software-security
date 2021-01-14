22f4267ae332cdfd9db79783ab2eb46d1a9f0e99
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-01-14 17:07:10 +0100

    Backend: get logs of a user

--- backend/routes/travelLogs.js
+++ backend/routes/travelLogs.js
@@ -1,9 +1,15 @@
+const checkUser = require('../middleware/checkUser');
 const jwtCheck = require('../middleware/jwtCheck');
+const TravelLog = require('../models/TravelLog');
 
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.json({ msg: 'Get all travel logs' });
+router.get('/', jwtCheck, checkUser, async (req, res) => {
+  console.log(req.loggedInUser);
+  console.log(req.loggedInUser.sub);
+  const { _id } = req.loggedInUser;
+  const logs = await TravelLog.find({ owner: _id })
+  res.json({ msg: 'Get all travel logs', data: logs });
 });
 
 router.post('/', jwtCheck, (req, res) => {
