8e202d6500eed23904315ce41f10ebfa19a8d814
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-01-18 18:06:39 +0100

    Backend: remove commented code

--- backend/routes/travelLogs.js
+++ backend/routes/travelLogs.js
@@ -7,12 +7,6 @@ const corsOptions = require('../utils/corsOptions');
 
 const router = require('express').Router();
 
-// router.options('/', (req, res) => {
-//   res.header('Access-Control-Allow-Origin', ['http://localhost:4000', 'http://localhost:3000']);
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-//   res.send(200);
-// })
 
 /**
  * GET - /travelLogs
