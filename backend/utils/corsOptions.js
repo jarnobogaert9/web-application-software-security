c9f95d0731146c2ce45bf90efa33b378c56173cb
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-01-24 14:10:32 +0100

    try with origin

--- backend/utils/corsOptions.js
+++ backend/utils/corsOptions.js
@@ -2,5 +2,5 @@ module.exports = {
   allowedHeaders:
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, CSRF-Token, X-CSRF-Token",
   credentials: true,
-  // origin: process.env.APP_ORIGIN,
+  origin: process.env.APP_ORIGIN,
 };
