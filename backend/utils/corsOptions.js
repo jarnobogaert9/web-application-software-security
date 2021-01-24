3fc3a11a809249bfa0c662cbd8870c7e73335041
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-01-24 14:07:10 +0100

    Allow all origins

--- backend/utils/corsOptions.js
+++ backend/utils/corsOptions.js
@@ -2,7 +2,5 @@ module.exports = {
   allowedHeaders:
     "Origin, X-Requested-With, Content-Type, Accept, Authorization, CSRF-Token, X-CSRF-Token",
   credentials: true,
-  origin: process.env.APP_ORIGIN,
-  // origin: 'https://www.travelr.jarnobogaert.com'
-  // origin: 'http://localhost:3000'
+  // origin: process.env.APP_ORIGIN,
 };
